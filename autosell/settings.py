3c823ac527533fe0d04a8747b4e80e34a6785e12
LaVitalle <vitorhugoinacio@outlook.com>
2025-09-01 22:35:47 -0300

    Fix: Secret

--- autosell/settings.py
+++ autosell/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-=bs=k@-41u=+^a36^b7+3+wj51^(-b$pw$#l(+=zdmvpjez&7o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 PROD = config('PROD')
