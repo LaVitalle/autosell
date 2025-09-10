c252717cc1a42e05311984e80934349e224c3f3b
LaVitalle <vitorhugoinacio@outlook.com>
2025-09-10 20:24:44 -0300

    refactor: Remove a aplicação 'evolutionapi' das configurações do Django no arquivo settings.py.

--- autosell/settings.py
+++ autosell/settings.py
@@ -62,7 +62,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'evolutionapi',
     'corsheaders',
     'dashboard',
     'products',
