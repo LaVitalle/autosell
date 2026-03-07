5c2fedc39ac15bfba98cbc5145693c7d88ab060d
LaVitalle <vitorhugoinacio@outlook.com>
2026-03-07 12:55:20 -0300

    fix - static() fix.

--- autosell/urls.py
+++ autosell/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.static import serve
 from dashboard import views
 
 urlpatterns = [
@@ -17,7 +17,6 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # Media files
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
